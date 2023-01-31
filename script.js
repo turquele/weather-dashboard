fa595708a9baa02a9255bf17dfab644eab6176f6
Jose Alonso <jose.m.alonso.g@gmail.com>
2023-01-31 01:49:57 +0000

    forecast added in cards

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // queryURL is the url we'll use to query the API
 var apiKey = "12cdccffdab81e42eb35554f2442ce5f"
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=" + apiKey;
-
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var cityInput
 
 //Search Button listener
@@ -19,19 +19,50 @@ $("#search-button").on("click", function(event) {
   }).then(function(response) {
     var lat = response[0].lat;
     var lon = response[0].lon;
-    console.log("Lat:" + lat + "Lon:" + lon)
+    console.log("Lat:" + lat + "Lon:" + lon);
+    //search weather for coordinates
     queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=` + apiKey + "&units=metric";
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
      console.log(response)
-     var currentWeather = response.list[0];
-     $("#today").append(`<h2>${currentWeather.weather[0].icon}</h2>`);
-     $("#today").append(`<h3>${currentWeather.weather[0].description}</h3>`);
-     $("#today").append(`<p>Temperature: ${currentWeather.main.temp}°C</p>`);
-     $("#today").append(`<p>Humidity: ${currentWeather.main.humidity}%</p>`);
-     $("#today").append(`<p>Wind speed: ${currentWeather.wind.speed}`+ " m/s</p>");
+     var currentWeather = response;
+     //Transform DT time
+     var timestamp = 1000*currentWeather.list[0].dt;
+     const date = new Date(timestamp);
+     // Populate weather in html
+     $("#today").empty();
+     $("#today").append(`
+      <div class="card">
+        <h5 class="card-header">${cityInput}: Now</h5>
+        <div class="card-body">
+          <h5 class="card-title">${currentWeather.list[0].weather[0].description}</h5>
+          <img class="card-img" src="http://openweathermap.org/img/wn/${currentWeather.list[0].weather[0].icon}@2x.png" style="width: 12rem;" alt="${currentWeather.list[0].weather[0].description}">
+          <p class="card-text">Temperature: ${currentWeather.list[0].main.temp}°C</p>
+          <p class="card-text">Humidity: ${currentWeather.list[0].main.humidity}%</p>
+          <p class="card-text">Wind speed: ${currentWeather.list[0].wind.speed} m/s</p></p>
+          <div class="card-footer text-muted">Current weather ${date}</div>
+        </div>
+      </div>`
+      );
+      $("#forecast").empty();
+      for (let i = 7; i < 40; i+=8) {
+        var timestamp = 1000*currentWeather.list[i].dt;
+        const date = new Date(timestamp);
+        $("#forecast").append(`
+        <div class="card text-white bg-primary mb-3" style="max-width: 20%;">
+          <div class="card-header"><h5>${days[date.getDay()]}</h5></div>
+          <div class="card-body">
+          <h5 class="card-title">${currentWeather.list[i].weather[0].description}</h5>
+          <img class="card-img" src="http://openweathermap.org/img/wn/${currentWeather.list[i].weather[0].icon}@2x.png" style="width: 12rem;" alt="${currentWeather.list[0].weather[0].description}">
+          <p class="card-text">Temperature: ${currentWeather.list[i].main.temp}°C</p>
+          <p class="card-text">Humidity: ${currentWeather.list[i].main.humidity}%</p>
+          <p class="card-text">Wind speed: ${currentWeather.list[i].wind.speed} m/s</p></p>
+          </div>
+        </div>
+        `);
+      }
     });
   });
 
@@ -43,12 +74,6 @@ $("#search-button").on("click", function(event) {
 
 
 
-
-
-//search weather for coordinates
-
-
-// Populate weather in htmel
 
 
 // Get 5 day forecast
